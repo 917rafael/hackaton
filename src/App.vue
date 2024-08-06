32be227854dc97375b2749bd8602ea5b7da0d407
917rafael <rpilger917@gmail.com>
2024-08-06 20:11:19 -0300

    criaçao da feat:#3  tentativa de criaçao de um carrossel

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import carousel from '@/components/Carousel.vue'
+
+</script>
+
+<template>
+  <main>
+    <RouterView />
+  </main>
+  <carousel />
+  
+</template>
+
+<style scoped>
+
+
+</style>
